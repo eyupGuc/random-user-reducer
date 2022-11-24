59cfe54315dfbc7fd5bd715cc6f8221c80a9d01a
eyupGuc <meydanguresi@gmail.com>
2022-11-24 08:50:10 +0300

    data

--- src/component/useReducer.jsx
+++ src/component/useReducer.jsx
@@ -10,25 +10,33 @@ export const UseRandomReducer = () => {
 
   const getUser = async () => {
     const url = "https://randomuser.me/api/";
+    setLoading(true);
     try {
       const res = await fetch(url);
       const data = await res.json();
-      console.log(data.results[0].picture.large);
-      setUser(data.results[0].picture.large);
+      console.log(data.results[0]);
+      console.log(data.results[0]);
+
+      setUser(data.results[0]);
+      setError("");
     } catch (error) {
       console.log(error);
+      setError("DATA CAN NOT BE FETCHED");
+      setUser("")
+    } finally {
+      setLoading(false);
     }
-    console.log(user);
   };
 
   return (
-    <Container fluid>
+    <Container className="w-50">
       <Row className="text-center  m-5">
-        <Button variant="warning" onClick={getUser} disabled={false}>
+        <Button variant="warning" onClick={getUser} disabled={loading}>
           GET USER
         </Button>
         {error && <h2>{error}</h2>}
-        {user && <img src={user}/>}
+        {user?.picture?.large && <img width="300px" src={user?.picture?.large} />}
+        {user?.email && <p> Email: {user.email}</p>}
       </Row>
     </Container>
   );
